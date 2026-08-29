8085d78e8dcad913bf2e64c8a9c89e0e643f8d0d
Anshuman <165463239+anshumansrivastavrv@users.noreply.github.com>
2026-08-29 16:22:18 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int prev1=0;
+        int prev2=0;
+
+        for(int i=0;i<cost.length;i++){
+            int curr=cost[i]+Math.min(prev1,prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        return Math.min(prev1,prev2);
+    }
+}
